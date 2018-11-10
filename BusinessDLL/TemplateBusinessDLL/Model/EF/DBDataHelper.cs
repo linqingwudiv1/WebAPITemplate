bd481498c8c3d65a80afafb5052133cdc440532a
小黑屋的懒慢 <aa875191946@qq.com>
2018-11-11 01:17:43 +0800

    up

--- BusinessDLL/TemplateBusinessDLL/Model/EF/DBDataHelper.cs
+++ BusinessDLL/TemplateBusinessDLL/Model/EF/DBDataHelper.cs
@@ -6,12 +6,20 @@ using System.Threading.Tasks;
 
 namespace AuditSystemBusinessDLL.Model.EF
 {
+    /// <summary>
+    /// 数据测试
+    /// </summary>
     public class DBDataHelper : IDataHelper
     {
         public DBDataHelper()
         {
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public dynamic GetData(params object[] args)
         {
             string arg_str = string.Empty;
